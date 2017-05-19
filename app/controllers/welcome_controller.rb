c7bc6db98030a87cf6941d0f11f43c65cba5dee4
Rozen90 <mixerka24@gmail.com>
2017-05-19 09:43:12 +0300

    second update

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,36 +1,30 @@
-require "csv"
-
 class WelcomeController < ApplicationController
 
   def index
   	@a = current_model.shops rescue []
-  	z = @a.map {|v| v.products.where(breadcrumb: "Суши")}.flatten
-  	@c = Kaminari.paginate_array(z).page(params[:page]).per(10)
-  	#s = CSV.read("/home/rozen/parsing_items/Crawlers/ProSushi/prosushi.by.csv",{headers: true})
-    #@s = s.headers 
-     respond_to do |format|
-      format.html {@c }
-      format.json {render json: @c, status: 200}
+    @cat = category.map {|ds| ds[:breadcrumb]}
+    breadcr = params[:category] || @cat.join(", ").gsub(', ',"','")
+    type = params[:type] || "price"
+    if @a.empty?
+      @c = []
+    else
+      @c = @a.select('products.*,shops.*').joins(:products).where("products.breadcrumb in ('#{breadcr}')").order("#{type} desc").page(params[:page]).per(10)
     end
-  end
-
-  def delit()
-  	@a = current_model.shops rescue []
-  	@cat = category.map {|ds| ds[:breadcrumb]}
-  	breadcr = params[:category] || @cat.join(", ").gsub(', ',"','")
-  	type = params[:type] || "price"
-  	@c = @a.select('products.*,shops.*').joins(:products).where("products.breadcrumb in ('#{breadcr}')").order("#{type} desc").page(params[:page]).per(10)
+    @cat_name = category_name
     respond_to do |format|
       format.html {@c }
       format.json {render json: @c, status: 200}
     end
   end
 
-
-
   private
 
   def category()
   	cat = Product.select("distinct(products.breadcrumb)")
   end
+
+  def category_name()
+    params[:category] ? params[:category] : "All"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do 
   get 'welcome/index'
-  get 'welcome/delit'
   devise_for :models
   root 'welcome#index'
 
